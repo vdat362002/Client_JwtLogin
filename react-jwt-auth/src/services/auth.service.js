21af101728a483352a6fb996d877961d891c072e
vdat362002 <vandatdinh2@gmail.com>
2023-09-07 17:36:14 +0700

    test

--- react-jwt-auth/src/services/auth.service.js
+++ react-jwt-auth/src/services/auth.service.js
@@ -4,11 +4,18 @@ const API_URL = "https://node-js-jwt-auth-mongodb-gules.vercel.app/api/auth/";
 
 class AuthService {
   login(username, password) {
-    return axios
-      .post(API_URL + "signin", {
-        username,
-        password
-      })
+    return axios({
+      url: API_URL + 'signin',
+      data: {
+        username: username,
+        password: password
+      },
+      method:'POST',
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+    })
       .then(response => {
         if (response.data.accessToken) {
           localStorage.setItem("user", JSON.stringify(response.data));
